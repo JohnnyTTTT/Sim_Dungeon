e771b0dd2c865f3a9326b693dea022c5701e0aec
CQ <caoqiang@vrcollab.com.cn>
2025-09-11 12:43:50 +0800

    Update Test Scene

--- Assets/UnityApp/Scripts/MyDungeonListener.cs
+++ Assets/UnityApp/Scripts/MyDungeonListener.cs
@@ -2,6 +2,7 @@ using DungeonArchitect;
 using DungeonArchitect.Builders.GridFlow;
 using DungeonArchitect.Flow.Domains.Tilemap;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -13,16 +14,23 @@ namespace Johnny.SimDungeon
         public GridFlowDungeonConfig dungeonConfig;
         public GridFlowDungeonModel dungeonModel;
         public GridFlowDungeonBuilder gridFlowDungeonBuilder;
+        public GridFlowDungeonQuery gridFlowDungeonQuery;
+        public GridFlowMinimap gridFlowMinimap;
 
         [SerializeField] private Camera m_Camera;
         [SerializeField] private GameObject m_HighlightPrefab;
         [SerializeField] private LayerMask m_GroundMask;
+        [SerializeField] private Transform m_SelectionRoot;
+
+
 
         private GameObject m_CurrentHighlight;
         private Vector3 m_LastCellPosition;
-        private List<IntVector2> m_CustomFloors = new List<IntVector2>();
+        private HashSet<IntVector2> m_CustomFloors = new HashSet<IntVector2>();
+        private List<GameObject> m_CurrentAreaHighlights = new List<GameObject>();
         private void Start()
         {
+            m_CustomFloors.Clear();
             dungeon.Build();
             //m_GridFlowDungeonBuilder.BuildDungeon(m_DungeonConfig, m_DungeonModel);
         }
@@ -43,29 +51,22 @@ namespace Johnny.SimDungeon
             {
                 var cellPos = SnapToGrid(hit.point);
                 var cell = dungeonModel.GetTilemapCell(cellPos);
-                if (cell != null && cell.CellType == FlowTilemapCellType.Custom )
+                if (cell != null && (cell.CellType == FlowTilemapCellType.Custom || cell.CellType == FlowTilemapCellType.Wall))
                 {
-                    Debug.Log(cell.CustomCellInfo.name);
-                    if (m_CurrentHighlight == null)
-                    {
-                        m_CurrentHighlight = Instantiate(m_HighlightPrefab, cellPos, Quaternion.identity);
-                        m_LastCellPosition = cellPos;
-                    }
-                    else
-                    {
-                        if (cellPos != m_LastCellPosition)
-                        {
-                            m_CurrentHighlight.transform.position = cellPos;
-                            m_LastCellPosition = cellPos;
-                        }
-                    }
+                    cellPos = new Vector3(cellPos.x, 4.01f, cellPos.z);
+                }
+                Debug.Log(cell.CellType + " " + cell.NodeCoord.x + "-" + cell.NodeCoord.y);
+                if (m_CurrentHighlight == null)
+                {
+                    m_CurrentHighlight = Instantiate(m_HighlightPrefab, cellPos, Quaternion.identity, m_SelectionRoot);
+                    m_LastCellPosition = cellPos;
                 }
                 else
                 {
-                    if (m_CurrentHighlight != null)
+                    if (cellPos != m_LastCellPosition)
                     {
-                        Destroy(m_CurrentHighlight);
-                        m_CurrentHighlight = null;
+                        m_CurrentHighlight.transform.position = cellPos;
+                        m_LastCellPosition = cellPos;
                     }
                 }
             }
@@ -81,15 +82,38 @@ namespace Johnny.SimDungeon
 
         private void CheckClick()
         {
-            if (Mouse.current.leftButton.wasPressedThisFrame && m_CurrentHighlight != null)
-            {
-                var cell = dungeonModel.GetTilemapCell(m_LastCellPosition);
-                Debug.Log(cell.NodeCoord.ToVector2());
-                Debug.Log(cell.TileCoord.ToVector2());
-                Debug.Log(cell.CellType);
-                //m_LastClickedCell = cell.TileCoord;
-                dungeon.Build();
-            }
+            if (m_CurrentHighlight != null)
+                if (Mouse.current.leftButton.wasPressedThisFrame)
+                {
+                    var cell = dungeonModel.GetTilemapCell(m_LastCellPosition);
+                    Debug.Log(cell.NodeCoord.ToVector2());
+                    Debug.Log(cell.TileCoord.ToVector2());
+                    Debug.Log(cell.CellType);
+                    m_CustomFloors.Add(cell.TileCoord);
+                    //m_LastClickedCell = cell.TileCoord;
+                    dungeon.Build();
+                }
+                else if (Mouse.current.rightButton.wasPressedThisFrame)
+                {
+                    if (m_CurrentAreaHighlights.Any())
+                    {
+                        for (int i = m_CurrentAreaHighlights.Count - 1; i >= 0; i--)
+                        {
+                            Destroy(m_CurrentAreaHighlights[i]);
+                        }
+                        m_CurrentAreaHighlights.Clear();
+                    }
+
+                    var cell = dungeonModel.GetTilemapCell(m_LastCellPosition);
+                    var cells = gridFlowDungeonQuery.GetLayoutNodeTile(cell.NodeCoord, false);
+                    foreach (var item in cells)
+                    {
+                        var position = gridFlowDungeonQuery.TileCoordToWorldCoord(item.TileCoord);
+                        position += new Vector3(0f, 0.01f, 0f);
+                        var current = Instantiate(m_HighlightPrefab, position, Quaternion.identity, m_SelectionRoot);
+                        m_CurrentAreaHighlights.Add(current);
+                    }
+                }
         }
 
 
@@ -100,18 +124,23 @@ namespace Johnny.SimDungeon
             var gridSize = dungeonConfig.gridSize;
             var x = Mathf.FloorToInt(value.x / gridSize.x) * gridSize.x + 2f;
             var z = Mathf.FloorToInt(value.z / gridSize.z) * gridSize.z + 2f;
-            return new Vector3(x, 0f, z);
+            return new Vector3(x, 0.01f, z);
         }
 
 
         public override void OnPostDungeonLayoutBuild(Dungeon dungeon, DungeonModel mode)
         {
-            //if (m_LastClickedCell != null)
-            //{
-            //    var cell = dungeonModel.Tilemap.Cells.GetCell(m_LastClickedCell.Value.x, m_LastClickedCell.Value.y);
-            //    cell.CellType = FlowTilemapCellType.Floor;
-            //}
+            if (m_CustomFloors != null && m_CustomFloors.Any())
+            {
+                foreach (var item in m_CustomFloors)
+                {
+                    var cell = dungeonModel.Tilemap.Cells.GetCell(item.x, item.y);
+                    cell.CellType = FlowTilemapCellType.Floor;
+                }
+            }
+            gridFlowMinimap.Initialize();
         }
+
         public override void OnPostDungeonBuild(Dungeon dungeon, DungeonModel model)
         {
             //Debug.Log("Dungeon build complete");
